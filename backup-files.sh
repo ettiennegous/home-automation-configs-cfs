f0be15649b692a95eb26aa8cca0cc7ffaa40465f
ettiennegous <ettiennegous@hotmail.com>
2019-06-23 20:59:22 +1000

    Add backup shell script to move all the files to a USB stick for just in case

---
+++ backup-files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+BACKUP_SOURCE="/home/ettienne/docker"
+MOUNT_POINT="/media/ettienne/BACKUP-STIK"
+
+#run a differential backup of files
+/usr/bin/rsync -avh --exclude node_modules --exclude home-assistant_v2.db "$BACKUP_SOURCE" "$MOUNT_POINT"
+exit
