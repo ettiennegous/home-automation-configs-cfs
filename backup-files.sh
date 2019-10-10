54ef2f53e54f76aeb1d9d175b60282d063ea5a1c
ettiennegous <ettiennegous@hotmail.com>
2019-10-10 22:33:43 +1100

    Update the backup script usb device location

--- backup-files.sh
+++ backup-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BACKUP_SOURCE="/home/ettienne/docker"
-MOUNT_POINT="/media/ettienne/BACKUP-STIK"
+MOUNT_POINT="/media/BACKUP-STIK"
 
 #run a differential backup of files
 /usr/bin/rsync -avh --exclude node_modules --exclude home-assistant_v2.db "$BACKUP_SOURCE" "$MOUNT_POINT"
